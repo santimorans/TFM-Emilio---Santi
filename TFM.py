bcce8a67c646ffdfbf5203d8e00233dca5ac45e3
santimorans <103330182+santimorans@users.noreply.github.com>
2022-09-14 01:02:28 +0200

    Add files via upload

---
+++ TFM.py
@@ -0,0 +1,9 @@
+# Aquí vamos a hacer el esqueleto del TFM. En primer lugar sin redes.
+
+# Paso 1: incluir los datos en el script
+
+# Paso 2: Determinar hacia donde va el mercado
+
+# Paso 3: calcular volatilidad y determinar hacia donde va
+
+# Paso 4: determinar cuando comprar y vender opciones. 
