e72b2b415926ee5dbdcb8129aa96f502656888a1
emiliohv <emiliohv@hotmail.es>
2022-09-14 21:12:35 +0200

    prueba control de cambios

--- TFM.py
+++ TFM.py
@@ -1,5 +1,5 @@
 # Aquí vamos a hacer el esqueleto del TFM. En primer lugar sin redes.
-
+#Esto es sólo una prueba de que funciona el control de cambios
 # Paso 1: incluir los datos en el script
 
 # Paso 2: Determinar hacia donde va el mercado
